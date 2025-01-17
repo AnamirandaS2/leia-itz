17a1cecadc924c5a6cfa1cd35f1a2acdec5fb97d
Ana Clara <anaclarapontesm@hotmail.com>
2025-01-17 17:44:40 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="w-full border-2">
+        <p className="uppercase">oiii caralho</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
