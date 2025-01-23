ebd78a0c82b26f2d27782d1b0dd2c7681e278585
Ana Clara <anaclarapontesm@hotmail.com>
2025-01-23 16:55:23 -0300

    fix image

--- src/pages/Landing/LandingContainer.tsx
+++ src/pages/Landing/LandingContainer.tsx
@@ -1,19 +1,19 @@
 import { Button } from "../../components/Button/Button";
-import "./Container/Landing.css";
 import wave from "../../assets/Vector.svg";
+import boyForms from "../../assets/boyForms.png";
+
 export default function LandingContainer() {
   return (
-    <div className="relative min-h-screen w-screen flex flex-col items-start justify-center  ">
-      <div
-        style={{
-          backgroundImage: `url(${wave})`,
-          backgroundSize: "cover",
-          objectFit: "cover"
-        }}
-        className="absolute inset-0 overflow-x-hidden w-full  z-[1]"
-      ></div>
-
-      <div className="fixed top-1/2 left-[15%] translate-x-[-50%] translate-y-[-50%] flex flex-col items-start text-left text-white max-w-xl pl-40  ">
+    <div
+      style={{
+        backgroundImage: `url(${wave})`,
+        backgroundSize: "contain",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "top center",
+      }}
+      className="flex flex-col min-h-screen overflow-y-auto"
+    >
+      <div className="flex flex-col items-start text-left text-white max-w-xl pl-40 py-20">
         <h1 className="text-3xl md:text-4xl font-bold text-purple mb-4">
           Transforme a leitura em uma experiência incrível!
         </h1>
@@ -24,7 +24,25 @@ export default function LandingContainer() {
         <div className="w-[45%]">
           <Button color="purple"> Começar agora! </Button>
         </div>
+        <div
+          className=" flex  items-center h-40 justify-evenly gap-20 rounded-2xl bg-gradient-to-r from-yellow to-pink mt-[1650px] "
+          style={{ padding: "4rem 2rem" }}
+        >
+          <div className="">
+            <img src={boyForms} className="object-contain w-40 h-52" alt="" />
+          </div>
+          <div className="text-center self-end text-white w-[1000px] mb-6">
+            <div className="w-[50%] ">
+              <p className="text-xl text-left font-semibold">
+                Não é estudade do IFMA, mas gostaria de fazer parte dessa
+                iniciativa?
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
+
+      <div style={{ height: "10vh" }}></div>
     </div>
   );
 }
